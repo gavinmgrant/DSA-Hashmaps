aa366477e508ce9c365d84365400970ab9709ee0
Gavin Grant <gavingrant@gmail.com>
2020-10-26 10:28:07 -0700

    fix(separate chaining): correct conflict resolution

--- SeparateChaining.js
+++ SeparateChaining.js
@@ -6,79 +6,54 @@
 
 // create a nodes class to be used for linked lists within the hashmap
 class _Node {
-    constructor(value, next) {
+    constructor(value) {
         this.value = value;
-        this.next = next;
+        this.next = null;
     }
 };
 
 class HashMap {
-    constructor(initialCapacity=8) {
-        this.length = 0;
+    constructor() {
         this._hashTable = [];
-        this._capacity = initialCapacity;
-        this._deleted = 0;
+    }
+
+    set(value) {
+        let key = HashMap._hashString(value);
+        let node = new _Node(value);
+        // if the hash table slot already has a value, 
+        // move that value to the next node in the linked list
+        if (this._hashTable[key]) {
+            node.next = this._hashTable[key];
+        }
+        // insert the new node into the hash table
+        this._hashTable[key] = node;
     }
 
     get(key) {
-        const index = this._findSlot(key);
-        if (this._hashTable[index] === undefined) {
-            throw new Error('Key error');
+        let hash = HashMap._hashString(key);
+        let chain = this._hashTable[hash];
+
+        if (!this._hashTable[hash]) {
+            return null;
         }
-        return this._hashTable[index].value;
+
+        if (chain.hasOwnProperty(key)) {
+            return chain[key]
+        }
+
+        return null;
     }
-
-    set(key, value) {
-        const loadRatio = (this.length + this._deleted + 1) / this._capacity;
-        if (loadRatio > this.MAX_LOAD_RATIO) {
-            this._resize(this._capacity * this.SIZE_RATIO)
-        }
-
-        const index = this._findSlot(key);
-        // refactor set method to use nodes at each slot within the hash table
-        let node = new _Node(value);
-
+    
+    delete(item) {
+        let key = HashMap._hashString(item);
         if (this._hashTable[key]) {
-            // assign current node to the next position
-            node.next = this._hashTable[key];
-        }
-        // assign the the new node to the current position
-        this._hashTable[key] = node;
-
-    }
-
-    delete(key) {
-        const index = this._findSlot(key);
-        const slot = this._hashTable[index];
-        if (slot === undefined) {
-            throw new Error('Key error');
-        }
-        slot.DELETED = true;
-        this.length--;
-        this._deleted++;
-    }
-
-    _findSlot(key) {
-        const hash = HashMap._hashString(key);
-        const start = hash % this._capacity;
-
-        for (let i = start; i < start + this._capacity; i++) {
-            const index = i % this._capacity;
-            return index;
-        }
-    }
-
-    _resize(size) {
-        const oldSlots = this._hashTable;
-        this._capacity = size;
-
-        this.length = 0;
-        this._deleted = 0;
-        this._hashTable = [];
-
-        for (const slot of oldSlots) {
-            if (slot !== undefined && !slot.DELETED) {
-                this.set(slot.key, slot.value);
+            console.log(this._hashTable[key]);
+            if (this._hashTable[key].value === item) {
+                // if the input string matches the first item in the bucket, 
+                // assign the next as current
+                this._hashTable[key] = this._hashTable[key].next;
+            } else {
+                return null;
             }
         }
     }
@@ -112,6 +87,7 @@ const separateChaining = () => {
     lotr.set("LadyOfLight", "Galadriel");
     lotr.set("HalfElven", "Arwen");
     lotr.set("Ent", "TreeBeard");
+    lotr.delete("Maiar");
     
     return lotr;
 }
